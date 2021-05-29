956f10b2712f6078f6c7b7a4ff9c53a029769dda
kmrshubham123 <kmrshubham123@gmail.com>
2021-05-29 23:18:46 +0530

    [Kumar Shubham]Added: Single Player at start position 0

--- SnakeLadderGame/SnakeLadderGame/Program.cs
+++ SnakeLadderGame/SnakeLadderGame/Program.cs
@@ -7,6 +7,8 @@ namespace SnakeLadderGame
         static void Main(string[] args)
         {
             Console.WriteLine("=======> WELCOME TO THE SNAKE AND LADDER GAME <=======");
+            SnakeLadder snake = new SnakeLadder();
+            snake.StartGame();
         }
     }
 }
---
+++ SnakeLadderGame/SnakeLadderGame/SnakeLadder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeLadderGame
+{
+    class SnakeLadder
+    {  /// <Single_player_at_position_0>
+      /// 
+      /// </summary>
+        public void StartGame()
+        {   //variables
+            int Position = 0; 
+            int PlayerOne;
+            
+            //initialize player
+            PlayerOne = Position;
+            Console.WriteLine($"PLAYER ONE: {PlayerOne}");
+        }
+
+       
+
+    }
+    
+}
